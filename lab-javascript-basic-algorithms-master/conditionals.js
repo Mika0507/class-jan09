432e78668840f31fda5e31ffa2e77f34cc042f2d
Belen Alonso <alonsoperalbelen@gmail.com>
2023-01-17 11:40:44 +0100

    fixed exercise js loops

--- lab-javascript-basic-algorithms-master/conditionals.js
+++ lab-javascript-basic-algorithms-master/conditionals.js
@@ -1,60 +1,66 @@
-// let hacker1 = "Belén";
+let hacker1 = "Belén";
 
-// let hacker2 = "Edge";
+let hacker2 = "Edge";
 
-// console.log(`The driver's name is  ${hacker1}`);
+console.log(`The driver's name is  ${hacker1}`);
 
-// console.log(`The navigator's name is ${hacker2}` );
-
-
-
-// if(hacker1.length > hacker2.length){
-//     console.log(`The driver has the longest name, it has ${hacker1.length}  characters.`);
-// } else if(hacker2.length > hacker1.length){
-//     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters. `);
-// }else{
-//     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
-// }
+console.log(`The navigator's name is ${hacker2}` );
 
 
 
-// Ejercicio Entradas cine
-let edad = parseInt(prompt("¿Cuántos años tienes?:"));
-
-
-
-if(edad <= 5){
-    console.log("Tiene entrada gratis");
-}else if(edad > 5 && edad < 15){
-    console.log("Tienes un 20% de descuento");
-}else if(edad >= 15 && edad <=60 ){
-    console.log("Pagan el precion normal");
-}else if(edad > 60){
-    console.log("Tienes un 40% de descuento");
-};
-
-
-
-
-const yourName = prompt("¿Cuál es tu nombre?:");
-const action = prompt("Pide un acción (saludar, despedirse, preguntar, gritar, descansar");
-
-switch(action){
-    case "saludar":
-        alert(`Hello, ${yourName}!`);
-        break;
-    case "despedirse":
-        alert(`Bye bye ${yourName}!`);
-        break;
-    case "preguntar":
-        alert(`How are you today, ${yourName}?`);
-        break;
-    case "gritar":
-        alert(`AHHHHHHHHHH!!!!!!!!!`);
-        break;
-    case "Descansar":
-        alert(`I'm signing off now.`)
-    default:
-        alert("I don't know how to do that yet, I'm just a little program :(");
-        break;
+if(hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length}  characters.`);
+} else if(hacker2.length > hacker1.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters. `);
+}else{
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 }
+
+// TERNARY OPERATOR
+// ----------------------------
+
+hacker1.length > hacker2.length ? console.log(`The driver has the longest name, it has ${hacker1.length}  characters.`) : console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters. `);
+
+
+hacker1.length > hacker2.length ? console.log(`The driver has the longest name, it has ${hacker1.length}  characters.`) : hacker1.length === hacker2.length ? console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`) : console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters. `);
+
+// // Ejercicio Entradas cine
+// let edad = parseInt(prompt("¿Cuántos años tienes?:"));
+
+
+
+// if(edad <= 5){
+//     console.log("Tiene entrada gratis");
+// }else if(edad > 5 && edad < 15){
+//     console.log("Tienes un 20% de descuento");
+// }else if(edad >= 15 && edad <=60 ){
+//     console.log("Pagan el precion normal");
+// }else if(edad > 60){
+//     console.log("Tienes un 40% de descuento");
+// };
+
+
+
+
+// const yourName = prompt("¿Cuál es tu nombre?:");
+// const action = prompt("Pide un acción (saludar, despedirse, preguntar, gritar, descansar");
+
+// switch(action){
+//     case "saludar":
+//         alert(`Hello, ${yourName}!`);
+//         break;
+//     case "despedirse":
+//         alert(`Bye bye ${yourName}!`);
+//         break;
+//     case "preguntar":
+//         alert(`How are you today, ${yourName}?`);
+//         break;
+//     case "gritar":
+//         alert(`AHHHHHHHHHH!!!!!!!!!`);
+//         break;
+//     case "Descansar":
+//         alert(`I'm signing off now.`)
+//     default:
+//         alert("I don't know how to do that yet, I'm just a little program :(");
+//         break;
+// }
--- section03/masloops.js
+++ section03/masloops.js
@@ -1,36 +1,85 @@
-// Loops 1
-// Crea un for loop que cuente de 0 a 100 y en cada iteración sume el valor actual a un resultado total. Al final haz un console.log que printee el resultado.
+// // Loops 1
+// // Crea un for loop que cuente de 0 a 100 y en cada iteración sume el valor actual a un resultado total. Al final haz un console.log que printee el resultado.
+// console.log("Loop 1");
+// let sumaNum = 0;
 
 // let contador;
 
-// for(contador = 0; contador < 101; contador++){
-//     console.log(contador);
+// for(contador = 0; contador <= 100; contador++){
+//     sumaNum += contador;
 // }
 
+// console.log(sumaNum);
 
 
 
+// // Loops 2
+// // Crea un array de números llamado "numbers" con valores aleatorios. Utiliza un for loop para recorrer el array y encontrar el número mayor y el número menor. Imprime ambos números en la consola.
+// console.log("Loop 2");
+// const numbers = [-8700,-7600,-6500, -6900, -5400, -4300, -3200, -2100, -1010, -9900, -2100];
 
-// Loops 2
-// Crea un array de números llamado "numbers" con valores aleatorios. Utiliza un for loop para recorrer el array y encontrar el número mayor y el número menor. Imprime ambos números en la consola.
+// let numBigger = Number.NEGATIVE_INFINITY;
+// let numMinor = Number.POSITIVE_INFINITY;
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // console.log(numBigger);
 
-for ( let num = 0; num <= numbers.length; num++){
-    console.log(`-. ${numbers[num]}`);
-}
+// for ( let num = 0; num <= numbers.length; num++){
+//     if(numbers[num] > numBigger){
+//         numBigger = numbers[num];
+//     }
+//     if(numbers[num]< numMinor){
+//         numMinor = numbers[num];
+//     }
+// }
+
+// console.log(numBigger);
+// console.log(numMinor);
+
+// console.log("Loop 2.1");
+// const numbers1 = [222, 87, 54, 687, 99, 10, 41, 325, 54, 76, 20];
+// let numBigger0 = 0;
+// let numMinor0 = Number.POSITIVE_INFINITY;
+
+// // console.log(numBigger);
+
+// for ( let num = 0; num <= numbers1.length; num++){
+//     if(numbers1[num] > numBigger0){
+//         numBigger0 = numbers1[num];
+//     }
+//     if(numbers1[num]< numMinor0){
+//         numMinor0 = numbers1[num];
+//     }
+// }
+
+// console.log(numBigger0);
+// console.log(numMinor0);
 
 
 
 
 // Loops 3
 // Utiliza un bucle while para imprimir los números pares del 2 al 20.
+// console.log("Loop 3");
 
-let numPares;
+// // Declaro una vaariable donde ver los números que queremos
+// let numPares = 20;
+// // Con esta variable le estoy diciendo que empiece en 2
+// let i = 2;
 
-while(){
+// // dentro del bucle tenemos «i <= numPares» aquí le estamos diciendo que cuando el numero introducido (la variable i) sea menor o igual al número introducido, entonces se escribe en el console y hacemos que salga la variable i
+// while(i <= numPares){
+//     console.log(i += 2);
+// }
 
-}
+// // Otra forma de resolver el while loop
+
+// let evenNumber = 2;
+
+// while(evenNumber <= 20){
+//     console.log(evenNumber);
+//     evenNumber += 2;
+// }
+
 
 
 
@@ -40,3 +89,16 @@ while(){
 // Crea una variable llamada "numeroIngresado" y asignale el valor 0.
 // Utiliza un bucle do...while para asignar a la variable "numeroIngresado" un número entero obtenido de un array de números [1,2,3,4,5,6,7,8,9,10]. El bucle debe continuar hasta que "numeroIngresado" sea igual a "numeroAdivinar".
 // Una vez que el bucle finaliza, imprime en la consola el número de iteraciones realizadas.
+
+let numEros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let numeroAdivinar = 5;
+let numeroIngresado = 0;
+let numeroIntentos;
+
+do{
+    numeroIngresado = numEros[numeroIntentos];
+    numeroIntentos++
+}while(numeroIngresado != numeroAdivinar)
+
+console.log(`${numeroAdivinar} se ha asivinado en ${numeroIntentos}`);
