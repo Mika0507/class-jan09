d09960fbd05087c173b2e06d3f463a41fe2e12cf
Belen Alonso <alonsoperalbelen@gmail.com>
2023-01-30 16:06:36 +0100

    vue class

---
+++ section-04-vue/vue-first-project/README.md
@@ -0,0 +1,56 @@
+# vue-first-project
+
+This template should help get you started developing with Vue 3 in Vite.
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+SFC === Single File Component === Componente de archivo único
+
+.vue
+Belén.vue
+BelénInfo.vue
+
+3 elementos
+html ---
+css ---
+js ---
+
+html === <template>
+ja === <script>
+css === <style>
+
+    #### Info acerca del elemento "style"
+
+    Cuando tengamos la palabra "scoped" dentro del "opening tag/etiqueta de apertura" significa que ese CSS/ estilos solo se aplican a este archivo!
+
+    #### Estructura de un componente vue v-composition api
+    
+    <!-- HTML aquí -->
+    <template></template>
+    <!-- JS aquí -->
+    <script></script>
+    <!-- CSS aquí -->
+    <style scoped></style>
---
+++ section-04-vue/vue-first-project/src/components/vueBasics/0-MyFirstComponent.vue
@@ -0,0 +1,62 @@
+  <!-- HTML aquí -->
+    <template>
+    
+        <div>
+
+            <p class="belen">Mi nombre es Belén, nací en España y tengo 34 años</p>
+
+            <p>{{ myNameTwo }}</p>
+
+        </div>
+
+        <div>
+            <p>
+                Mi nombre es {{ myName }}, nací en {{ countryOfBirth }} y tengo {{ age }} años {{ booleanValue }}
+            </p>
+        </div>
+        <section>
+            <div class="counter">
+                <button @click="decreaseCounter"> - </button>
+                <p>{{ counter.count }}</p>
+                <button @click="increaseCounter"> + </button>
+            </div>
+        </section> 
+        
+    <!-- HTML puesto con interpolación de texto - mejor conocido como nuestro claudators de bigote -->
+    </template>
+
+
+    <!-- JS aquí -->
+    <script setup>
+        import { ref, reactive} from "vue";
+
+
+        const myNameTwo = "Belén";
+
+        const myName = ref("Belén");
+        const countryOfBirth = ref("España");
+        const age = ref(34);
+
+
+        const myInfo = reactive({
+            myName: "Belén",
+            countryOfBirth: "España",
+            age: 34,
+        })
+
+        const counter = reactive({
+            count: 0,
+            title: "My counter",
+        })
+        const increaseCounter = () => {
+            counter.count++
+        }
+        const decreaseCounter = () => {
+            counter.count--
+        }
+    </script>
+
+
+    <!-- CSS aquí -->
+    <style scoped>
+    </style>
