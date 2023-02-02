96926e686622b3fbdededa8928bebbbd93f14271
Belen Alonso <alonsoperalbelen@gmail.com>
2023-02-02 19:36:02 +0100

    exercise vue finished

---
+++ section-04-vue/vue-exercise-router/src/views/Bands.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+    import {ref, reactive} from "vue";
+
+    const imageSrc = ref ("https://phantom-telva.unidadeditorial.es/ea62e7b06405306e4902e406746b520e/crop/0x63/1497x962/resize/828/f/jpg/assets/multimedia/imagenes/2020/03/16/15843755691245.jpg");
+
+    const imageAlt = ref("Lady GAGA")
+
+    const boolean = ref (true);
+
+    const verImage = () => {
+        boolean.value = !boolean.value
+    }
+</script>
+
+
+<template>
+
+    <div>
+        <h1>Lady Gaga</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit earum, minus reiciendis ex animi aperiam, optio ut quaerat modi, voluptas sint necessitatibus iure odit officiis enim maiores fugiat esse quas?</p>
+        <button @click="verImage">Click me to see the image</button>
+        <img :src="imageSrc" :alt="imageAlt" :class= "boolean ? 'vista' : 'no-vista'"/>
+    </div>
+    
+</template>
+
+<style scoped>
+
+    .no-vista{
+        display: none;
+    }
+    .vista{
+        width: 200px;
+    }
+</style>
---
+++ section-04-vue/vue-exercise-router/src/views/Food.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+    import {ref, reactive} from "vue";
+
+
+    const food = reactive({
+        imageSrc:"https://offloadmedia.feverup.com/madridsecreto.co/wp-content/uploads/2022/02/02043605/Ruta-sushi3-1024x720.jpg",
+        imageAlt: "bandeja de sushi",
+        boolean: true,
+    })
+
+    const verImage = () => {
+        food.boolean = !food.boolean
+    }
+</script>
+
+
+<template>
+
+    <div>
+        <h1>Sushi</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit earum, minus reiciendis ex animi aperiam, optio ut quaerat modi, voluptas sint necessitatibus iure odit officiis enim maiores fugiat esse quas?</p>
+        <button @click="verImage">Click me to see the image</button>
+        <div v-if="food.boolean">
+            <img :src="food.imageSrc" 
+            :alt="food.imageAlt" 
+            class= "vista"/>
+        </div>
+        
+    </div>
+    
+</template>
+
+<style scoped>
+
+    .no-vista{
+        display: none;
+    }
+    .vista{
+        width: 200px;
+    }
+</style>
