84eff340984990eab9c8d4cb54a8489ed195819d
Belen Alonso <alonsoperalbelen@gmail.com>
2023-02-15 15:03:11 +0100

    lab wikicoutries

---
+++ vue-lab-day-four/src/App.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import NavigationHeader from "./components/NavigationHeader.vue";
+</script>
+
+<template>
+  <div class="app">
+    <header>
+      <NavigationHeader />
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
